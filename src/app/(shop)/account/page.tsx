2b69a0ba5e9a352d4e684dcf44562e23c4b9280c
Jatin <jatin.parate@mlveda.com>
2023-11-25 21:10:51 +0530

    added account page

--- src/app/(shop)/account/page.tsx
+++ src/app/(shop)/account/page.tsx
@@ -1,3 +1,24 @@
-export default function AccountPage() {
-  return <h1>Account Page</h1>;
+export default async function AccountPage() {
+  const create = async (formData: FormData) => {
+    "use server";
+    console.log("formData", Object.fromEntries(formData.entries()));
+  };
+  const todos = await fetch("https://jsonplaceholder.typicode.com/todos").then(
+    (res) => res.json()
+  );
+
+  return (
+    <div>
+      <form action={create}>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <ul>
+        {todos.map((todo: any) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
